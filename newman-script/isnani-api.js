dc148e608b64a8b99cbddb524c9b89cc41d8634b
isnani <61511717+Isnani25@users.noreply.github.com>
2024-08-25 16:24:49 +0700

    remove file

--- newman-script/isnani-api.js
+++ newman-script/isnani-api.js
@@ -1,7 +1,7 @@
 const newman = require("newman")
 
 newman.run({
-    collection: "json-collection/isnani-API Project.postman_collection.json",
-    environment: "json-env/Isnani-regress.postman_environment.json",
+    collection: "json-collection/isnani2-API Project.postman_collection.json",
+    environment: "json-env/Isnani2_Token.postman_environment.json",
     reporters: ["cli", "htmlextra"]
 })
